eb83f7d0ef8874f5cbeae2540db523ced42bd446
shreyatiwari15122007-oss <shreyatiwari.15122007@gmail.com>
2026-07-31 02:21:08 +0530

    Create Notification.js

---
+++ backend/models/Notification.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema(
+  {
+    receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    title: { type: String, required: true },
+    message: { type: String, required: true },
+    type: {
+      type: String,
+      enum: ["new_food", "new_request", "request_accepted", "request_rejected", "pickup_confirmed", "system"],
+      default: "system",
+    },
+    relatedFood: { type: mongoose.Schema.Types.ObjectId, ref: "Food" },
+    relatedRequest: { type: mongoose.Schema.Types.ObjectId, ref: "Request" },
+    read: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Notification", notificationSchema);
