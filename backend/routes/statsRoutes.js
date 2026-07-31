eaa44f5a1b441a09f1c0d247d3e2d9a4f875bf9c
himanshujain1109-hash <himanshujain1109@gmail.com>
2026-07-31 09:54:31 +0530

    Delete backend directory

--- backend/routes/statsRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getPublicStats } = require("../controllers/statsController");
-
-router.get("/public", getPublicStats);
-
-module.exports = router;
